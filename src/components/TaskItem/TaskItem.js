02569f4dae011f74f7b9fe04f3f7607a5266d794
atomlab <aldrinthomaslab@gmail.com>
2020-09-03 21:52:39 +0800

    Subscribed TaskList component to context changes

--- src/components/TaskItem/TaskItem.js
+++ src/components/TaskItem/TaskItem.js
@@ -1,27 +1,130 @@
 import React, { Component } from 'react';
+import TodoListContext from './../../context/TodoListContext';
 import './taskItem.css';
 
-const TaskItem = () => {
-	return (
-		<div className="taksItem row">
+class TaskItem extends Component {
+	static contextType = TodoListContext;
+
+	constructor(props) {
+		super(props);
+
+		let { id, title } = props;
+
+		this.state = {
+			id,
+			title,
+			edit: false,
+		};
+	}
+
+	handleTick = () => {
+		this.context.toggleStatus(this.state.id);
+	}
+
+	handleDelete = () => {
+		this.context.deleteItem(this.state.id);
+	}
+
+	toggleEdit = () => {
+		this.setState({ edit: !this.state.edit });
+	}
+
+	handleOnchange = e => {
+		let { value } = e.target;
+
+		this.setState({ title: value });
+	}
+
+	handleOnKeyUp = e => {
+		if (e.keyCode === 13) {
+			this.context.editItem(this.state.id, this.state.title);
+			this.toggleEdit();
+		}
+	}
+
+	toggleTextAndInput = () => {
+		if (this.state.edit) {
+			return (
+				<input 
+					type="text" 
+					value={this.state.title} 
+					className="form-control"
+					onChange={this.handleOnchange}
+					onKeyUp={this.handleOnKeyUp}
+				/>
+			)
+		}
+
+		return this.getItemText();
+	}
+
+	getItemText = () => {
+		let className = "";
+
+		if (this.props.isDone) {
+			className = "text--done";
+		}
+
+		return (
+			<h4
+				className={className}
+				title={this.state.title}
+			>
+				{this.state.title}
+			</h4>
+		);
+	}
+
+	render() {
+		return (
+			<div className="taskItem row">
 				<div className="col-1">
-					<input type="checkbox" className="form-control" />
+					<input 
+						type="checkbox" 
+						checked={this.props.isDone}
+						className="form-control"
+						value={this.props.id}
+						onChange={this.handleTick}
+					/>
 				</div>
-				<div className="col-9">
-					<h4>This is a sample</h4>
+				<div className="col-6 title-container">
+					{this.toggleTextAndInput()}
 				</div>
-				<div className="col-1">
-					<button className="btn">
-						Edit
-					</button>
+
+				<div class="col-5">
+					{ this.state.edit && (
+						<button 
+							className="btn btn-secondary"
+							onClick={this.toggleEdit}
+						>
+							Cancel
+						</button>
+					)}
+
+					{ !this.state.edit && (
+							<button 
+								className="btn btn-success edit-btn"
+								onClick={this.toggleEdit}
+								disabled={this.props.isDone}
+							>
+								Edit
+							</button>
+					)}
+					
+					{ !this.state.edit && (
+							<button 
+								className="btn btn-danger"
+								onClick={this.handleDelete}
+								disabled={this.props.isDone}
+							>
+								Delete
+							</button>
+					)}
 				</div>
-				<div className="col-1">
-					<button className="btn">
-						Delete
-					</button>
-				</div>
+				
 			</div>
-	);
+		);
+	}
 };
 
 export default TaskItem;
--- src/components/TaskList/TaskList.js
+++ src/components/TaskList/TaskList.js
@@ -1,13 +1,32 @@
 import React, { Component } from 'react';
 import TaskItem from './../TaskItem/TaskItem';
+import TodoListContext from './../../context/TodoListContext';
 import './taskList.css';
 
-const TaskList = () => {
-	return (
-		<div className="taskList container">
-			<TaskItem />
-		</div>
-	);
+class TaskList extends Component {
+	render() {
+		return (
+			<TodoListContext.Consumer>
+				{ ({list}) => (
+							<div className="taskList">
+								<div className="taskList container">
+									{ list.length === 0 && <h4>List is empty</h4> }
+									{ list.map((item) =>(
+											<TaskItem 
+												key={item.id} 
+												id={item.id} 
+												title={item.title} 
+												isDone={item.done}
+											/>
+										))
+									}
+								</div>
+							</div>
+						)
+				}
+			</TodoListContext.Consumer>
+		);
+	}
 };
 
 export default TaskList;
